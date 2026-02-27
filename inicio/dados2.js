23285b03c999937023b7185c4bb5c467d72575ed
tds_sesi <toffolia61@gmail.com>
2026-02-27 11:37:15 -0300

    tipos de dados em js

---
+++ inicio/dados2.js
@@ -0,0 +1,13 @@
+// console.log(19.9 * 0.6);
+
+var preco = 19.9;
+var desconto = 0.6;
+var precoComDesconto = preco * (1 - desconto);
+// console.log(precoComDesconto);
+
+let nome = "Caderno";
+let categoria = "papelaria";
+console.log("produto: " + nome +
+", categoria: " + categoria +
+", preço: " + preco +
+",desconto: " + desconto);
---
+++ inicio/dados3.js
@@ -0,0 +1,17 @@
+let idade = 15;
+console.log(typeof idade);
+console.log(typeof 15);
+
+let valor = 1500.50;
+console.log(typeof valor);
+
+let estaChovendo =false; // true
+console.log(typeof estaChovendo);
+
+let nome = "lucas";
+console.log(typeof nome);
+
+console.log(1 + 1);
+console.log("1" + 1);
+
+console.log(valor * "valor");
