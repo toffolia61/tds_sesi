c75d3cb3fe750433af05769c76e1d994cefc93eb
toffolia61 <toffolia61@gmail.com>
2026-04-16 11:27:41 -0300

    atividade.js

--- array/array2.js
+++ array/array2.js
@@ -1,3 +1,13 @@
-const nome = [];
+
+
 
 // Desafio: inserir t novos índices com 5 nomes e imprimir na tela
+
+const nome = [];
+nome[0] = "js";
+nome[1] = "uncini";
+nome[2] = "lucao";
+nome[3] = "chico";
+nome[3] = "issoai";
+console.log(nome);
+console.log("\n" + nome.length);
---
+++ array/array3.js
@@ -0,0 +1,9 @@
+console.log(typeof console);
+console.log(typeof console.log);
+
+const numeros = [10, 20, 30];
+console.log(typeof numeros);
+
+numeros.push(40); // Adcionar no fim do array
+numeros[4] = 50;
+console.log(numeros);
